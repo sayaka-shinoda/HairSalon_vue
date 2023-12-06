df9e1b3aa0b8ccfc6c49dfee7e9e33a61d0fcbcb
shinoda <s.saya2x4@gmail.com>
2023-12-06 15:47:48 +0900

    Add. Logo img.

---
+++ hairsalon_project/src/components/Header.vue
@@ -0,0 +1,42 @@
+<script setup>
+    import { ref } from 'vue'
+</script>
+
+<template>
+
+    <head>
+
+        <meta name="viewport" content="width=device-width" />
+
+    </head>
+
+    <body>
+
+        <img src="../assets/hairsalon_logo.png" alt="logo" class="logo_img" />
+
+    </body>
+
+</template>
+
+<style scoped>
+    /* mobile */
+    @media(max-width: 480px){
+        
+    .logo_img{
+        display: block;
+        width: 45px;
+        height: 45px; 
+        margin: auto;
+    }
+    }
+    /* pc */
+    @media(min-width: 481px){
+        
+    .logo_img{
+        display: block;
+        width: 120px;
+        height: 120px; 
+        margin: auto;
+    }
+    }
+</style>
