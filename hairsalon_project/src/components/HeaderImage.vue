f089966254d552cb2327a0f7ca3d3b4c553b4e38
shinoda <s.saya2x4@gmail.com>
2023-12-07 11:39:42 +0900

    Add. Main img.

---
+++ hairsalon_project/src/components/HeaderImage.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+
+    <head>
+
+        <meta name="viewport" content="width=device-width" />
+
+    </head>
+
+    <body>
+
+        <img src="../assets/hairsalon_main.png" alt="main" class="main_img" />
+
+    </body>
+
+</template>
+
+<style scoped>
+
+    .main_img {
+
+	    display: block;
+        width: 100%;
+ 	    height: auto;
+
+    }
+
+</style>
