f47f4f2adcf4d4b6455b6e14139a192b3fc3faa7
shinoda <s.saya2x4@gmail.com>
2023-12-08 16:22:22 +0900

    Add. Slide img.

--- hairsalon_project/src/components/Gallery.vue
+++ hairsalon_project/src/components/Gallery.vue
@@ -4,6 +4,21 @@
 
     const gallery = ref('GALLERY')
 
+    /* slide img */
+    import gallery_img1 from '@/assets/gallery_img1.jpg';
+    import gallery_img2 from '@/assets/gallery_img2.jpg';
+    import gallery_img3 from '@/assets/gallery_img3.jpg';
+    import gallery_img4 from '@/assets/gallery_img4.jpg';
+    import gallery_img5 from '@/assets/gallery_img5.jpg';
+  
+    const images = ref([
+        { src: gallery_img1 },
+        { src: gallery_img2 },
+        { src: gallery_img3 },
+        { src: gallery_img4 },
+        { src: gallery_img5 },
+    ]);
+
 </script>
 
 <template>
@@ -22,6 +37,21 @@
 
         </div>
 
+        <!-- slide img -->
+        <div class="slide_container">
+
+            <div class="slides">
+
+                <div v-for="(image, index) in images" :key="index" class="slide">
+
+                <img :src="image.src" alt="slide img" />
+
+                </div>
+
+            </div>
+
+        </div>
+
     </body>
 
 </template>
@@ -33,10 +63,36 @@
         text-align: center;
     }
 
+    .slide_container {
+        position: relative;
+        max-width: 80%;
+        overflow: hidden;
+    }
+  
+    .slides {
+        display: flex;
+        animation: moveSlides 20s infinite linear;
+        width: 80%; /* img size */
+    }
+  
+    .slide {
+        flex: 0 0 calc(50% - 15px);
+    }
+  
+    .slide img {
+        width: 100%;
+        height: auto;
+    }
+  
+    @keyframes moveSlides {
+        0% { transform: translateX(0); }
+        100% { transform: translateX(-100%); }
+    }
+
     /* mobile */
-    @media(max-width: 480px){
+    @media(max-width: 480px) {
         
-        h1{
+        h1 {
             display: inline-block;
             position: relative;
             text-align: center;
@@ -59,12 +115,20 @@
             z-index: -1;
         }
 
+        .slide_container {
+            margin: 25px auto;
+        }
+
+        .slides {
+            gap: 10px;
+        }
+
     }
 
-        /* pc */
-    @media(min-width: 481px){
+    /* pc */
+    @media(min-width: 481px) {
 
-        h1{
+        h1 {
             display: inline-block;
             position: relative;
             margin-top: 80px;
@@ -85,6 +149,14 @@
             z-index: -1;
         }
     
+        .slide_container {
+            margin: 60px auto;
+        }
+
+        .slides {
+            gap: 15px;
+        }
+
     }
 
 </style>
